6c9ef7e128921e78776bf7b6bb2a97ff50544372
Natasha Vyas <natasha.vyas@digi-prex.com>
2024-03-22 13:03:13 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: `/home`,
+    pathMatch: 'full'
+  },
+  {
+    path: `home`,
+    component: HomeComponent
+  },
+  {
+    path: `menu`,
+    component: MenuComponent
+  },
+  {
+    path: `contact`,
+    component: ContactComponent
+  },
+  {
+    path: `about`,
+    component: AboutComponent
+  },
+  {
+    path: `blogs`,
+    component: BlogsComponent
+  },
+  {
+    path: `post/:name`,
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
